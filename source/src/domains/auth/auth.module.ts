d9e88b27dfe21a34acf355b802e80154df26a49e
Chalermpon Dasri <chalermpon.dasri@dga.or.th>
2025-06-12 23:45:57 +0700

    remove unused import

--- source/src/domains/auth/auth.module.ts
+++ source/src/domains/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { LoggerModule } from '../../core/logger.module'
 import { TokenizationModule } from '@domains/auth/tokenization.module'
 import { AuthController } from '@domains/auth/controllers/auth.controller'
 import { authenticationServiceProvider } from '@domains/auth/providers/authentication-service.provider'
