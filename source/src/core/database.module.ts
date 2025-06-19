2dc38905ea98ac2817a5c6391e3e722e8070c169
Chalermpon Dasri <chalermpon.dasri@dga.or.th>
2025-06-19 15:58:15 +0700

    reset to default

--- source/src/core/database.module.ts
+++ source/src/core/database.module.ts
@@ -1,11 +1,5 @@
-import {
-    Module,
-    OnApplicationShutdown,
-    OnModuleDestroy,
-} from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { databaseProvider } from '@core/providers/database.provider'
-import { ProviderName } from '@core/constants/provider-name.enum'
-import { ModuleRef } from '@nestjs/core'
 
 @Module({
     providers: [
@@ -15,22 +9,4 @@ import { ModuleRef } from '@nestjs/core'
         databaseProvider,
     ]
 })
-export class DatabaseModule implements OnApplicationShutdown, OnModuleDestroy {
-    constructor(private readonly moduleRef: ModuleRef) {}
-
-
-    async onModuleDestroy() {
-        console.log(`Database module destroyed`)
-
-        // Access the client through the database object if stored
-        console.log(this.moduleRef.get(ProviderName.MONGO_DATASOURCE))
-    }
-
-    async onApplicationShutdown(signal?: string) {
-        console.log(`Database module shutting down (${signal})`)
-
-        // Access the client through the database object if stored
-        console.log(this.moduleRef.get(ProviderName.MONGO_DATASOURCE))
-    }
-
-}
+export class DatabaseModule {}
